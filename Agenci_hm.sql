4347b13fb08b6c7be80bba9ed48d30eaaaaecc5c
asadbek000002 <asadbektuygunov9@gmail.com>
2023-03-31 18:14:33 +0500

    HM_file

---
+++ Agenci_hm.sql
@@ -0,0 +1,13 @@
+SET search_path = my_schema, "$user", entertainmentagencyexample;
+
+-- 1
+
+-- select (agtfirstname, agtlastname) as agent, agtphonenumber from agents
+ 
+--  2
+ 
+-- select * from engagements
+
+-- 3
+
+-- select * from engagements order by startdate, enddate desc , engagementnumber asc
---
+++ Recipes_hm.sql
@@ -0,0 +1,9 @@
+SET search_path = my_schema, "$user", recipesexample;
+
+-- 1
+
+-- select * from ingredients
+
+-- 2
+
+--select * from recipes order by recipetitle
